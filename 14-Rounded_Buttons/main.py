80c3ff9c95602ab4c4f25eece76eca53470a8872
Gtadia <m235humit9@gmail.com>
2022-08-15 22:51:55 -0400

    saving for the night

---
+++ 14-Rounded_Buttons/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder  
+from kivy.core.window import Window
+
+Builder.load_file("my_kivy_file.kv")
+
+class MyLayout(Widget):
+    pass
+
+class MyApp(App):   
+    def build(self):
+        Window.clearcolor = (1, 1, 1)
+        return MyLayout()
+
+
+if __name__ == "__main__":
+    MyApp().run()
---
+++ 15-Image_Viewer_With_FileChooserIconView_and_FileChooserListView/main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder  
+from kivy.core.window import Window
+
+Builder.load_file("my_kivy_file.kv")
+
+class MyLayout(Widget):
+    def my_selected(self, filename):
+        # This is a file that has been selected from the ui 
+        try:
+            self.ids.my_image.source = "self.ids."
+
+class MyApp(App):   
+    def build(self):
+        Window.clearcolor = (0, 0, 0)
+        return MyLayout()
+
+
+if __name__ == "__main__":
+    MyApp().run()
